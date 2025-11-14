75f1b63b5600d1d86e6541d7dfa6e2f9f938c69c
Jak <jahmedabeer05@gmail.com>
2025-11-14 17:07:14 +0600

    State as a Snapshot !important

--- README.md
+++ README.md
@@ -2,20 +2,8 @@
 
 Adding Interactivity
 
-[Render & Commit: How render works!](https://react.dev/learn/render-and-commit)
+[State as a Snapshot](https://react.dev/learn/state-as-a-snapshot)
 
 ## Info
 
-- Before your components are displayed on screen, they `must be rendered by React`.
-- Trigger a render (There are two reasons for a component to render)
-  - Initial render. (When your app starts, you need to trigger the initial render.)
-  - The component’s (or one of its ancestors’) state has been updated.
-- After you trigger a render, React calls your components to figure out what to display on screen. “Rendering” is React calling your components.
-  - `On initial render`, React will `call the root component`.
-  - For subsequent renders, React will call the function component `whose state update` triggered the render.
-  - `This process is recursive`: if the updated component returns some other component, React will render that component next, and if that component also returns something, it will render that component next, and so on.
-  - If the parent component renders, the child component inside it will render too.( Changes happen in VDOM )
-- Any screen update in a React app happens in three steps:
-  - Trigger
-  - Render
-  - Commit
+- A state variable’s value never changes within a render, even if its event handler’s code is asynchronous.
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
-function Image() {
-  return (
-    <img
-      src="https://i.imgur.com/ZF6s192.jpg"
-      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
-    />
-  );
-}
+import { useState } from 'react';
 
-export default function Gallery() {
+/*
+  Notice that number only increments once per click!
+  
+  Here is what this button’s click handler tells React to do:
+
+  1. setNumber(number + 1): number is 0 so setNumber(0 + 1).
+    - React prepares to change number to 1 on the next render.
+
+  2. setNumber(number + 1): number is 0 so setNumber(0 + 1).
+    - React prepares to change number to 1 on the next render.
+
+  3. setNumber(number + 1): number is 0 so setNumber(0 + 1).
+    - React prepares to change number to 1 on the next render.
+
+  Visualize this by mentally substituting state variables with their values in your code
+  `
+    <button onClick={() => {
+      setNumber(0 + 1);
+      setNumber(0 + 1);
+      setNumber(0 + 1);
+    }}>+3</button>
+  `
+*/
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+  const [numberOne, setNumberOne] = useState(0);
 
   return (
     <>
-      <section>
-        <h1>Inspiring Sculptures</h1>
-        <Image />
-        <Image />
-        <Image />
-      </section>
+      <div>
+        <h1>{number}</h1>
+        <button onClick={() => {
+          setNumber(number + 1);
+          setNumber(number + 1);
+          setNumber(number + 1);
+        }}>+3</button>
+      </div>
+      <div>
+        <h1>{numberOne}</h1>
+        <button onClick={() => {
+          setNumberOne(numberOne + 5); // 5 -> 10 -> 15
+          alert(numberOne); // 0 -> 5 -> 10
+        }}>+5</button>
+      </div>
     </>
-  );
+  )
 }
