41de482b86ef34e059fd63fee37688875a0c2b44
Jak <jahmedabeer05@gmail.com>
2025-11-14 13:37:17 +0600

    How render works
    Trigger
    Render
    Commit

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 Adding Interactivity
 
-[State: A Component's Memory](https://react.dev/learn/state-a-components-memory)
+[Render & Commit: How render works!](https://react.dev/learn/render-and-commit)
 
 ## Info
 
-- To `update a component with new data`, two things need to happen:
-  - Retain the data between renders.
-  - Trigger React to render the component with new data (re-rendering)
-- The `useState Hook` provides those two things:
-  - A state variable to retain the data between renders.
-  - A state setter function to update the variable and trigger React to render the component again.
-  - State is isolated and private
+- Before your components are displayed on screen, they `must be rendered by React`.
+- Trigger a render (There are two reasons for a component to render)
+  - Initial render. (When your app starts, you need to trigger the initial render.)
+  - The component’s (or one of its ancestors’) state has been updated.
+- After you trigger a render, React calls your components to figure out what to display on screen. “Rendering” is React calling your components.
+  - `On initial render`, React will `call the root component`.
+  - For subsequent renders, React will call the function component `whose state update` triggered the render.
+  - `This process is recursive`: if the updated component returns some other component, React will render that component next, and if that component also returns something, it will render that component next, and so on.
+  - If the parent component renders, the child component inside it will render too.( Changes happen in VDOM )
+- Any screen update in a React app happens in three steps:
+  - Trigger
+  - Render
+  - Commit
--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,22 @@
-import { useState } from 'react';
-import { sculptureList } from './data.js';
-
-
-function NotReactiveWay() {
-  /*
-    The handleClick event handler is updating a local variable, index. But two things prevent that change from being visible:
-
-      `Local variables don’t persist between renders`. When React renders this component a second time, it renders it from scratch—it doesn’t consider any changes to the local variables.
-      `Changes to local variables won’t trigger renders`. React doesn’t realize it needs to render the component again with the new data.
-  */
-  let index = 0;
-
-  function handleClick() {
-    index = index + 1;
-    console.log(index)
-  }
-  let sculpture = sculptureList[index];
+function Image() {
   return (
-    <>
-      <button onClick={handleClick}>Next</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
-    </>
-  )
-}
-
-function ReactiveWay() {
-  /*
-    To update a component with new data, two things need to happen:
-
-      `Retain the data between renders`.
-      `Trigger React to render the component with new data` (re-rendering).
-  */
-
-  const [index, setIndex] = useState(0);
-
-  function handleClick() {
-    index + 1 < sculptureList.length && setIndex(index + 1)
-  }
-  let sculpture = sculptureList[index];
-  return (
-    <>
-      <button onClick={handleClick}>Next</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
-    </>
-  )
+    <img
+      src="https://i.imgur.com/ZF6s192.jpg"
+      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
+    />
+  );
 }
 
 export default function Gallery() {
 
   return (
     <>
-      {/* <NotReactiveWay /> */}
-      <ReactiveWay />
-      <hr />
-      {/* State is isolated and private */}
-      <ReactiveWay />
+      <section>
+        <h1>Inspiring Sculptures</h1>
+        <Image />
+        <Image />
+        <Image />
+      </section>
     </>
   );
 }
