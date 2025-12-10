cc78efe3c3438ab37b980426fffa55327f646a4f
Jak <jahmedabeer05@gmail.com>
2025-12-10 18:01:06 +0600

    Reusing logic with: Custom Hook

--- src/components/CheckNetwork.jsx
+++ src/components/CheckNetwork.jsx
@@ -1,25 +1,7 @@
-import { useEffect, useState } from 'react'
+import useOnlineStatus from '../hooks/useOnlineStatus'
 
 export default function CheckNetwork() {
-    const [isOnline, setIsOnline] = useState(true)
-
-    useEffect(() => {
-        function handleOnline() {
-            setIsOnline(true)
-        }
-
-        function handleOffline() {
-            setIsOnline(false)
-        }
-
-        window.addEventListener('online', handleOnline)
-        window.addEventListener('offline', handleOffline)
-
-        return () => {
-            window.removeEventListener('online', handleOnline)
-            window.removeEventListener('offline', handleOffline)
-        }
-    }, [])
+    const isOnline = useOnlineStatus()
 
 
     return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>
---
+++ src/hooks/useOnlineStatus.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+function useOnlineStatus() {
+    const [isOnline, setIsOnline] = useState(true)
+
+    function handleOnline() {
+        setIsOnline(true)
+    }
+
+    function handleOffline() {
+        setIsOnline(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener('online', handleOnline)
+        window.addEventListener('offline', handleOffline)
+
+        return () => {
+            window.removeEventListener('online', handleOnline)
+            window.removeEventListener('offline', handleOffline)
+        }
+    }, [])
+
+  return isOnline;
+}
+
+export default useOnlineStatus
