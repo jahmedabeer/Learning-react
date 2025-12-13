536229af1923425f7340b214d72430109d368e19
Jak <jahmedabeer05@gmail.com>
2025-12-13 09:12:42 +0600

    spread and rest operator

--- scope/script.js
+++ scope/script.js
@@ -1,18 +1,31 @@
-// object methods
-const shortForm = 'Js'
-
+// spread operator
+const fruits = ['Banana', 'Orange']
 const lang = {
     name: 'Javascript',
     year: 1995,
-    creator: 'Brandan Erich',
-    shortForm  // property shorthand - equivalent to shortForm: shortForm
+    creator: 'Brendan Eich'
 }
 
-console.log(Object.keys(lang)) // returns an array of the object's property names
-console.log(Object.values(lang)) // returns an array of the object's property values
-console.log(Object.entries(lang)) // return an array of key value pairs(array)
+const result = [...fruits]
+result.push('Guava')
 
-const dynamicKey = 'popularity' // Dynamic property assignment using bracket notation
-lang[dynamicKey] = '100%';
+console.log(fruits)
+console.log(result)
 
+const result1 = {
+    ...lang,
+    shortForm: 'Js'
+}
 console.log(lang)
+console.log(result1)
+
+
+// rest operator
+function sum(text, ...rest){
+    // console.log(arguments)
+    // console.log(rest) // array
+
+    const result = rest.reduce((sum, cur) => sum + cur, 0)
+    console.log(`${text} ${result}`)
+}
+sum('Result:',5,6,8,9)
