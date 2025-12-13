72159820e4eab283df98c3b2ce6d43cda67b12e1
Jak <jahmedabeer05@gmail.com>
2025-12-13 20:56:14 +0600

    nullish | or operator | and operator

--- scope/script.js
+++ scope/script.js
@@ -1,12 +1,21 @@
-const user = {
-    name: 'jak',
-    age: 26,
-    education: {
-        degree: 'Graduate',
-        school: {
-            name : "X"
-        }
-    }
-}
+// Nullish Coalescing & Short Circuit Conditionals
 
-console.log(user?.education?.school?.name) // optional chaining
+// Nullish = Null / Undefined
+
+let lang = null
+let lang1 = "Python"
+console.log(
+    lang ?? "javascript",
+    lang1 ?? "javascript"
+) // if left side null / undefined return right side otherwise return left side
+
+let lang2 = false
+console.log(
+    lang2 || "javascript",
+    lang1 || "javascript"
+) // if left side falsy(6) return right side otherwise return left side
+
+console.log(
+    lang2 && "javascript",
+    lang1 && "javascript"
+) // if left side truthy return right side otherwise return left side
