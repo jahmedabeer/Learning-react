312590dc7b632b8a3940aeaa978f8ba8251be906
Jak <jahmedabeer05@gmail.com>
2025-12-13 10:23:02 +0600

    truthy and falsy value

--- scope/script.js
+++ scope/script.js
@@ -1,31 +1,8 @@
-// spread operator
-const fruits = ['Banana', 'Orange']
-const lang = {
-    name: 'Javascript',
-    year: 1995,
-    creator: 'Brendan Eich'
+// falsy values(6): false, 0, undefined, null, '', NaN | Everything else is truthy
+
+const result = undefined
+if(!result){ // if falsy
+    console.log(result)
+}else{
+    console.log(result)
 }
-
-const result = [...fruits]
-result.push('Guava')
-
-console.log(fruits)
-console.log(result)
-
-const result1 = {
-    ...lang,
-    shortForm: 'Js'
-}
-console.log(lang)
-console.log(result1)
-
-
-// rest operator
-function sum(text, ...rest){
-    // console.log(arguments)
-    // console.log(rest) // array
-
-    const result = rest.reduce((sum, cur) => sum + cur, 0)
-    console.log(`${text} ${result}`)
-}
-sum('Result:',5,6,8,9)
